9965a45c7199ccd0a0f983438b37cfc699cff0d7
lucabecci <beccibrian@gmail.com>
2021-06-20 13:28:04 -0300

    chore(Metrics): Good conf for metrics

--- src/services/MetricsService.js
+++ src/services/MetricsService.js
@@ -1,25 +1,38 @@
 const statusMonitor = require("express-status-monitor");
 module.exports = {
     server_metrics: function (app) {
-        app.use(statusMonitor());
+        app.use(statusMonitor(configuration));
     },
 };
 
 const configuration = {
-    healthChecks: [
+    title: "Core Status",
+    theme: "default.css", 
+    path: "/status",
+    spans: [
         {
-            protocol: "http",
-            host: "localhost", //modifu for prod
-            port: 4040,
-            path: "/health/ex1",
-            headers: {},
+            interval: 1, // Every second
+            retention: 60, // Keep 60 datapoints in memory
         },
         {
-            protocol: "http",
-            host: "localhost", //modifu for prod
-            port: 4040,
-            path: "/status/200",
-            headers: {},
+            interval: 5, // Every 5 seconds
+            retention: 60,
+        },
+        {
+            interval: 15, // Every 15 seconds
+            retention: 60,
         },
     ],
+    chartVisibility: {
+        cpu: true,
+        mem: true,
+        load: true,
+        eventLoop: false,
+        heap: false,
+        responseTime: true,
+        rps: true,
+        statusCodes: true,
+    },
+    healthChecks: [],
+    ignoreStartsWith: "/admin",
 };
